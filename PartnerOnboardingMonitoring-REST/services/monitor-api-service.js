43def5dcbbc17a5dfca2c9797196986429ebc48f
Wang <alexwang@lm-sjn-21005497.paypalcorp.com>
2016-07-07 10:52:10 -0700

    should now include payload when adding to mongodb. testing on c3

--- PartnerOnboardingMonitoring-REST/services/monitor-api-service.js
+++ PartnerOnboardingMonitoring-REST/services/monitor-api-service.js
@@ -8,6 +8,7 @@ var url = 'mongodb://partner-self-service-6103.ccg21.dev.paypalcorp.com:12345/';
 //var url = 'mongodb://localhost:12345/';
 var MongoClient = mongodb.MongoClient;
 var assert = require('assert');
+var async = require('async');
 
 var logSchema = new mongoose.Schema({
 		
@@ -60,10 +61,9 @@ module.exports = function module() {
 					if (details.records.length == 0) {
 						console.log("No results!");
 					} else {
-						//console.log("Details: " + JSON.stringify(details, null, 4));
-						var eventDetailURL = details.records[0].url;
-						var rawLogsURL = eventDetailURL.replace("eventDetail", "rawLogs");
-						callback(details, rawLogsURL);
+						//var eventDetailURL = details.records[0].url;
+						//var rawLogsURL = eventDetailURL.replace("eventDetail", "rawLogs");
+						callback(details);
 					}
 				} else {
 					console.log("Connection error when getting details: " + response.statusCode);
@@ -71,19 +71,45 @@ module.exports = function module() {
 			});
 		},
 
-		getRawLogs : function getRawLogs(details, url, callback) {
-			request(url, function(error, response, body){
-				if(!error && response.statusCode == 200) {
-					console.log("raw logs successfully retrieved!");
-					//console.log(body);
-					callback(details, body);
-				} else {
-					console.log("Network error in getRawLogs: " + response.statusCode);
-				}
+		getRawLogs : function getRawLogs(details, callback) {
+			db.on('error', console.error);
+			db.once('open', function() {
+				var Log = mongoose.model('Log', logSchema);
+
+				async.each(details.records, function(record, asyncCallback){
+					var eventDetailURL = record.url;
+					var rawLogsURL = eventDetailURL.replace("eventDetail", "rawLogs");
+					request(rawLogsURL, function(error, response, body){
+						if(!error && response.statusCode == 200) {
+							console.log("raw logs successfully retrieved!");
+							// create schema instance from record, add body
+							var toStore = new Log(record);
+							toStore.payload = body;
+							toStore.save(function(err, result){
+								console.log("Inserted Document: " + JSON.stringify(result));
+								asyncCallback();
+							});
+						} else {
+							console.log("Network error in getRawLogs: " + response.statusCode);
+						}
+					});
+				}, function(err){
+					db.close();
+					callback();
+				});
 			});
+			// request(url, function(error, response, body){
+			// 	if(!error && response.statusCode == 200) {
+			// 		console.log("raw logs successfully retrieved!");
+			// 		//console.log(body);
+			// 		callback(details, body);
+			// 	} else {
+			// 		console.log("Network error in getRawLogs: " + response.statusCode);
+			// 	}
+			// });
 		},
 
-		insertMongo: function insertMongo(details, payload) {
+		insertMongo: function insertMongo(record, payload) {
 			   mongoose.connect(url);
 
 				db.on('error', console.error);
@@ -102,73 +128,5 @@ module.exports = function module() {
 				});
 
 		}
-
-		// insertMongo: function insertMongo(details, payload) {
-		// mongoose.connect(url); 
-
-		//  	db.on('error', console.error);
-		//  	db.once('open', function() {
-
-		//  		var Log = mongoose.model('Log', logSchema);
-
-		//  		var sampleResponse = new Log(details.records[0]); // gets the first element from the list of responses (for testing)
-
-		//  		sampleResponse.payload = payload; // add payload onto the response JSON object
-
-		//  		//var sandbox_col = db.collection('sandbox_col');
-		//  		//var Test = mongoose.model('Test', testSchema);
-		// 		//var testResponse = new Test({TestKey : "Test Value"});
-		// 		//console.log(JSON.stringify(testResponse));
-
-		// 		sampleResponse.save(function(err, result) {
-		// 		//sandbox_col.save(testResponse, function(err, result) {
-		// 			//assert.equal(err, null);
-		// 			//console.log("Inserted Document Result: " + JSON.stringify(result));
-		// 			Log.find(function(err, logs){
-		// 				if (err) return console.error(err);
-		// 				console.dir(JSON.stringify(logs, null, 4));
-		// 				db.close();
-		// 			});
-		// 		});
-
-		// 		// sampleResponse.save(function(err, sampleResponse) { // save to mongoDB
-		 
-		// 		// 	 if (err) {
-		// 		// 		 return console.error(err);
-		// 		// 	 }
-
-		// 		// 	 console.log("Element inserted into mongoDB database : " + JSON.stringify(sampleResponse, null, 4));
-
-
-		// 		// 	 db.close();
-		// 		// });
-
-		// 	});
-
-		// 	// MongoClient.connect(url, function(err, db){
-		// 	// 	if(err) {
-		// 	// 		console.log('Unable to connect to the mongoDB server. Error:', err);
-		// 	// 	} else {
-		// 	// 		console.log('Connection established to', url);
-
-		// 	// 		var Log = mongoose.model('Log', logSchema);
-		// 	// 		var sampleResponse = new Log(details.records[0]);
-		// 	// 		sampleResponse.payload = payload;
-		// 	// 		//console.log(JSON.stringify(sampleResponse));
-
-		// 	// 		var Test = mongoose.model('Test', testSchema);
-		// 	// 		var testResponse = new Test({TestKey : "Test Value"});
-		// 	// 		console.log(JSON.stringify(testResponse));
-
-		// 	// 		var sandbox_col = db.collection('sandbox_col');
-		// 	// 		sandbox_col.save(testResponse, function(err, result) {
-		// 	// 			//assert.equal(err, null);
-		// 	// 			console.log("Inserted Document Result: " + JSON.stringify(result));
-		// 	// 			db.close();
-		// 	// 		});
-		// 	// 	}
-		// 	// });
-
-		// }
 	};
 };
