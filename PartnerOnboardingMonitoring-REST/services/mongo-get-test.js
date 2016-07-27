2630d75eee308267788c6fb8cd20a8be1708f786
Wang <alexwang@paypal.com>
2016-07-27 09:56:17 -0700

    mongo-get-test just saving toStore works when run locally

--- PartnerOnboardingMonitoring-REST/services/mongo-get-test.js
+++ PartnerOnboardingMonitoring-REST/services/mongo-get-test.js
@@ -3,7 +3,7 @@ var request = require('request'); // require request
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 var mongodb = require('mongodb');
-var url = 'mongodb://root:PQCtUUhZnbsmjbUH2C^QKYa3gnv7g2cDX^m9@10.25.39.2:27017';
+var url = 'mongodb://root:H9yu7Xn+WD!Ru6Dc_thvxtU7c7AKDuHy292x@10.25.39.2:27017';
 //var url = 'mongodb://root:PQCtUUhZnbsmjbUH2C^QKYa3gnv7g2cDX^m9@partner-self-service-2-9367.ccg21.dev.paypalcorp.com:27017';
 //var url = 'mongodb://localhost:12345/';
 var MongoClient = mongodb.MongoClient;
@@ -15,11 +15,18 @@ mongoose.connect(url);
 db.on('error', console.error);
 db.once('open', function() {
 
-	Log.findOne({ 'payload.Type' : 't'}, function (err, result) {
-		console.log("mongodb query returned!");
-		if (err) console.log(err);
-		console.log(JSON.stringify(result, null, 4));
+	var toStore = new Log({rawLogsURL : "www.fakeurl.com"});
+	toStore.save(function(err, result){
+		if(err) console.log(err);
+		console.log("Inserted Document: " + JSON.stringify(result));
 		db.close();
 	});
+
+	// Log.findOne({ 'payload.Type' : 't'}, function (err, result) {
+	// 	console.log("mongodb query returned!");
+	// 	if (err) console.log(err);
+	// 	console.log(JSON.stringify(result, null, 4));
+	// 	db.close();
+	// });
 
 });
