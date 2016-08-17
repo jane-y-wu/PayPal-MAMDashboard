1c3f558d354d30731a2436e6dd0434ac1f793d71
Wang <alexwang@paypal.com>
2016-08-17 10:39:57 -0700

    replaced app folder with single-page-app

---
+++ PartnerOnboardingMonitoring-app/src/js/actions/LogActions.js
@@ -0,0 +1,22 @@
+import dispatcher from "../dispatcher";
+
+var url = 'http://localhost:3003/api/getLogs/?startDate=2016-07-26T02:45:00&endDate=2016-07-29T11:00:00'
+
+export function getLogs() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = (e) => {
+    if (request.readyState !== 4) {
+      return;
+    }
+
+    if (request.status === 200) {
+      console.log('success', request.responseText);
+      dispatcher.dispatch({type: "UPDATE_LOGS", logs: request.responseText});
+    } else {
+      console.warn('error');
+    }
+  };
+
+  request.open('GET', url);
+  request.send();
+}
---
+++ PartnerOnboardingMonitoring-app/src/js/pages/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+export default class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+
+    return (
+      <div>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <AppBar
+            title="MAM API Monitoring Dashboard"
+            onLeftIconButtonTouchTap={this.handleToggle}
+          />
+        </MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <Drawer open={this.state.open} docked={false} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+            <MenuItem onTouchTap={this.handleClose}>Full Logs</MenuItem>
+            <MenuItem onTouchTap={this.handleClose}>Full Statistics</MenuItem>
+          </Drawer>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
