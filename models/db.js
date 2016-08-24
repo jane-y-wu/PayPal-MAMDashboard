8bc760c2cee4ad8d1389af14e635576a7c255be4
Jane Wu <janwu@paypal.com>
2016-08-23 17:29:34 -0700

    added a total line for the graph; mongoose connection closes when app ends

--- models/db.js
+++ models/db.js
@@ -11,6 +11,25 @@ mongoose.Promise = global.Promise;
 
 
 mongoose.connect(url, function(){
-    console.log('mongoose connected');
+    console.log('Mongoose connected');
 })
+
+
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', function() {  
+  mongoose.connection.close(function () { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+}); 
+
 module.exports = mongoose;
