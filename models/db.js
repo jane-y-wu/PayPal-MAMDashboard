10a6b664858e524176044027f0ff67f517fe2ee9
Jane Wu <janwu@paypal.com>
2016-08-24 10:43:27 -0700

    mongoose connection closes when app terminates

--- models/db.js
+++ models/db.js
@@ -9,10 +9,27 @@ var url = 'mongodb://10.25.39.2:27017/admin';
 mongoose.Promise = global.Promise;
 //mongoose.connect(url);
 
-
 mongoose.connect(url, {user: 'root', pass: 'fKMjMPjgF2jMQEdRx323euyqZMqzpCNB!KB6'}, function(){
     console.log('Mongoose Connected');
 });
+
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', function() {  
+  mongoose.connection.close(function () { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+}); 
+
 // mongoose.connect(url, function(){
 //     console.log('mongoose connected');
 // })
