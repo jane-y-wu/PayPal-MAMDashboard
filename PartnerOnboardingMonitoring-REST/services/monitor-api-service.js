fd7e942eb460c02aa53ba59190107005e10f8012
Jane Wu <janwu@paypal.com>
2016-08-24 09:54:12 -0700

    minor changes

--- PartnerOnboardingMonitoring-REST/services/monitor-api-service.js
+++ PartnerOnboardingMonitoring-REST/services/monitor-api-service.js
@@ -152,6 +152,7 @@ module.exports = function module() {
 										if(err) console.log(err);
 										console.log("Inserted Document: " + JSON.stringify(result));
 										async2Callback();
+									});
 
 									Log.findOne({ 'payload.Type' : 't'}, function (err, result) {
 										console.log("mongodb query returned!");
@@ -159,118 +160,19 @@ module.exports = function module() {
 										console.log(JSON.stringify(result, null, 4));
 										async2Callback();
 									});
+								
+								
+
 								} else if (currRecord["@Subclasstype"] != "calblockresponse") {
 									console.log("Unknown subclasstype: " + currRecord["@Subclasstype"]);
 								}
-							}
-						} else {
-							console.log(error);
-							console.log("Status Code: " + response.statusCode);
+								else {
+									console.log(error);
+									console.log("Status Code: " + response.statusCode);
+								}
+							} 
 						}
 					});
-
-					// var rawLogsURL = eventDetailURL.replace("eventDetail", "rawLogs");
-					//
-					// // TODO: decompose
-					// request(rawLogsURL, function(error, response, body){ // use an async each for each row in the response
-					// 	if(!error && response.statusCode == 200 && body !== "") {
-					// 		console.log("raw logs successfully retrieved!");
-					//
-					// 		var lines = body.split("\r\n");
-					// 		async.each(lines, function(singleLog, async2Callback){
-					// 			if (singleLog !== "") {
-					// 				var logSegments = singleLog.split("\t");
-					// 				//console.log(JSON.stringify(logSegments, null, 4));
-					// 				var match = logSegments[0].match(/[a-zA-Z]+/);
-					// 				logSegments[0] = logSegments[0].substring(match.index, logSegments[0].length);
-					// 				logSegments.unshift(logSegments[0][0]);
-					// 				logSegments[1] = logSegments[1].substring(1, logSegments[1].length);
-					// 				logSegments[4] = parseInt(logSegments[4]);
-					// 				var fields = ["Class", "Timestamp", "Type", "Name", "Status"]; //"Duration", "Data" | Duration is only a field with 't', 'T' or 'A' TODO: account for possibility of 'A'
-					//
-					// 				// if the line's name matches the array of valid names
-					// 				if (errorNames.indexOf(logSegments[3]) >= 0) {
-					// 					var localLog = { metaData : {}, payload: {} };
-					// 					// rawLogsURL from rawLogsURL
-					// 					localLog.rawLogsURL = rawLogsURL;
-					// 					// metaData object from record
-					// 					localLog.metaData["Machine"] = record.values.Machine;
-					// 					localLog.metaData["Pool"] = record.values.Pool;
-					// 					var dataCenter = "Data-Center";
-					// 					localLog.metaData["Data_Center"] = record.values[dataCenter];
-					//
-					// 					var dateMatch = rawLogsURL.match("datetime=(.*) ");
-					// 					var calendarDate = dateMatch[1];
-					//
-					// 					for (var field in fields) {
-					// 						switch(fields[field]) {
-					// 							case "Timestamp":
-					// 								var time = logSegments[field];
-					// 								var fullDate = calendarDate + 'T' + time.substring(0, 8);
-					// 								var fullDateDashes = fullDate.replace(/\//g, "-");
-					// 								localLog.payload["Full_Date"] = new Date(fullDateDashes);
-					// 								break;
-					// 							default:
-					// 								localLog.payload[fields[field]] = logSegments[field];
-					// 						}
-					// 					}
-					// 					//console.log(JSON.stringify(localLog));
-					//
-					// 					var payloadSegments = logSegments[5].split("&");
-					// 					//console.log(payloadSegments);
-					//
-					// 					for (var i in payloadSegments) { // skip duration field
-					// 						var split = payloadSegments[i].split("=");
-					// 						switch(split[0]) {
-					// 							case "Status":
-					// 								localLog.payload[split[0]] = parseInt(split[1]);
-					// 								break;
-					// 							case "isLoginable":
-					// 							case "hasPartnerRelationships":
-					// 								localLog.payload[split[0]] = (split[1] === 'true');
-					// 								break;
-					// 							default:
-					// 							localLog.payload[split[0]] = split[1];
-					// 						}
-					// 					}
-					// 					//console.log(JSON.stringify(localLog, null, 4));
-					//
-					// 					var toStore = new Log(localLog);
-					// 					console.log("toStore: " + JSON.stringify(toStore, null, 4));
-					//
-					// 					toStore.save(function(err, result){
-					// 						if(err) console.log(err);
-					// 						console.log("Inserted Document: " + JSON.stringify(result));
-					// 						async2Callback();
-					//
-					// 						// Log.findOne({ 'payload.Type' : 't'}, function (err, result) {
-					// 						// 	console.log("mongodb query returned!");
-					// 						// 	if (err) console.log(err);
-					// 						// 	console.log(JSON.stringify(result, null, 4));
-					// 						// 	async2Callback();
-					// 						// });
-					// 					});
-					//
-					// 					errorType = localLog.payload["Name"];
-					// 					date = localLog.payload["Full_Date"];
-					//
-					//
-					// 				} else {
-					// 					async2Callback();
-					// 				}
-					// 			} else {
-					// 				async2Callback();
-					// 			}
-					// 		}, function(err) {
-					//
-					// 			asyncCallback();
-					//
-					// 		});
-					// 	} else {
-					// 		//if (error) console.log("Network error in getRawLogs: " + response.statusCode); //TODO debug the errors being received
-					// 		asyncCallback();
-					// 	}
-					// });
 
 				}, function(err/*, numErrors, errorType, date*/){
 					//db.close();
